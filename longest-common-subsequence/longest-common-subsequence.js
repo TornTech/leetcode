9e4e02ec1011f1053c9948daaccd5f92548735af
Ben Torn <tornben@gmail.com>
2021-08-05 19:16:27 -0700

    Time: 128 ms (42.33%), Space: 53.4 MB (42.00%) - LeetHub

---
+++ longest-common-subsequence/longest-common-subsequence.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} text1
+ * @param {string} text2
+ * @return {number}
+ */
+var longestCommonSubsequence = function(A, B) {
+    // const memo = new Array(A.length + 1).fill(
+    //              new Array(B.length + 1).fill(null))
+    const memo = new Array(A.length + 1).fill(0).map(() => new Array(B.length + 1).fill(null));
+    
+    return longestCommonSubsequenceHelper(A, B, memo);
+};
+
+var longestCommonSubsequenceHelper = function(A, B, memo) {
+    let i = A.length;
+    let j = B.length;
+    
+    if (memo[i][j] === null) {
+        if (i === 0 || j === 0) {
+            memo[i][j] = 0
+        } else if (A[i-1] === B[j-1]) {
+            memo[i][j] = 1 + longestCommonSubsequenceHelper(A.substring(0,i-1), B.substring(0,j-1), memo);
+        } else {
+            memo[i][j] = Math.max(longestCommonSubsequenceHelper(A, B.substring(0,j-1), memo),
+                                  longestCommonSubsequenceHelper(A.substring(0,i-1), B, memo));
+        }
+    }
+
+    return memo[i][j];
+};
