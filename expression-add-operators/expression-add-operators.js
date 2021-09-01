c341528f161daddeab6a5a5d346018e894b958fe
Ben Torn <tornben@gmail.com>
2021-09-01 14:26:30 -0700

    Time: 330 ms (25.00%), Space: 48.7 MB (13.54%) - LeetHub

---
+++ expression-add-operators/expression-add-operators.js
@@ -0,0 +1,40 @@
+/**
+ * @param {string} num
+ * @param {number} target
+ * @return {string[]}
+ */
+var addOperators = function(num, target) {
+    const output = [];
+
+    operationCalculator(num, [], 0, 0)
+    
+    function operationCalculator(string, operatorsSoFar, sumSoFar, previous) {      
+        if (sumSoFar === target && !string.length) {
+            const result = operatorsSoFar.join('');
+            if (result.length >= num.length) {
+                output.push(result);
+                return;
+            }
+        }
+        const len = string[0] === '0' ? 1 : string.length;
+        
+        for (let i = 1; i <= len; i++) {
+            const currNum = Number(string.slice(0,i));
+            const restNum = string.slice(i);
+            
+            if (!operatorsSoFar.length) {
+                operationCalculator(restNum, [currNum], currNum, currNum)
+            } else {
+                const product = previous * currNum;
+                operationCalculator(restNum, [...operatorsSoFar, "*", currNum], sumSoFar - previous + product, product);
+                operationCalculator(restNum, [...operatorsSoFar, "+", currNum], sumSoFar + currNum, currNum);
+                operationCalculator(restNum, [...operatorsSoFar, "-", currNum], sumSoFar - currNum, 0-currNum);
+                
+            }
+        }
+    }
+    
+    return output;
+};
+
+
