de643ee96b5c0125164e6a20c7727efc933ecd9c
Ben Torn <tornben@gmail.com>
2021-10-03 23:29:09 -0700

    Time: 187 ms (64.84%), Space: 47.5 MB (78.39%) - LeetHub

--- kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.js
+++ kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.js
@@ -4,15 +4,11 @@ class KthLargest {
      * @param {number[]} nums
      */
     constructor(k, nums) {
-        this.nums = nums.sort((a,b) => a-b);
         this.k = k;
+        this.heap = new MinHeap(nums);
         
-        if (!nums.length) {
-            this.kthElement = -Infinity;
-        } else if (this.k > this.nums.length) {
-            this.kthElement = this.nums[0];
-        } else {
-            this.kthElement = nums[(nums.length - 1) - k + 1];
+        while (this.heap.size() > k) {
+            this.heap.poll();
         }
     };
     
@@ -21,27 +17,109 @@ class KthLargest {
      * @return {number}
      */
     add(val) {
-        this.nums.push(val);
-        this.nums.sort((a,b) => a-b);   
-        
-        if (this.k <= this.nums.length || val > this.kthElement) {
-            this.kthElement = this.nums[this.nums.length - this.k];
-        } else {
-            this.kthElement = this.nums[0]
-        }
-            
-        return this.kthElement;
+        this.heap.offer(val);
+        if (this.heap.size() > this.k)
+            this.heap.poll();
+        return this.heap.peek();
     }
-    
-        
 }
 
+class MinHeap {
+  constructor(data = []) {
+    this.data = data;
+    this.comparator = (a, b) => a - b;
+    this.heapify();
+  }
 
+  // O(nlog(n))
+  heapify() {
+    if (this.size() < 2) return;
+    for (let i = 1; i < this.size(); i++) {
+      this.bubbleUp(i);
+    }
+  }
+
+  // O(1)
+  peek() {
+    if (this.size() === 0) return null;
+    return this.data[0];
+  }
+
+  // O(log(n))
+  offer(value) {
+    this.data.push(value);
+    this.bubbleUp(this.size() - 1);
+  }
+
+  // O(log(n))
+  poll() {
+    if (this.size() === 0) return null;
+    const result = this.data[0];
+    const last = this.data.pop();
+    if (this.size() !== 0) {
+      this.data[0] = last;
+      this.bubbleDown(0);
+    }
+    return result;
+  }
+
+  // O(log(n))
+  bubbleUp(index) {
+    while (index > 0) {
+      const parentIndex = (index - 1) >> 1;
+      if (this.comparator(this.data[index], this.data[parentIndex]) < 0) {
+        this.swap(index, parentIndex);
+        index = parentIndex;
+      } else {
+        break;
+      }
+    }
+  }
+
+  // O(log(n))
+  bubbleDown(index) {
+    const lastIndex = this.size() - 1;
+    while (true) {
+      const leftIndex = index * 2 + 1;
+      const rightIndex = index * 2 + 2;
+      let findIndex = index;
+      if (
+        leftIndex <= lastIndex &&
+        this.comparator(this.data[leftIndex], this.data[findIndex]) < 0
+      ) {
+        findIndex = leftIndex;
+      }
+      if (
+        rightIndex <= lastIndex &&
+        this.comparator(this.data[rightIndex], this.data[findIndex]) < 0
+      ) {
+        findIndex = rightIndex;
+      }
+      if (index !== findIndex) {
+        this.swap(index, findIndex);
+        index = findIndex;
+      } else {
+        break;
+      }
+    }
+  }
+
+  // O(1)
+  swap(index1, index2) {
+    [this.data[index1], this.data[index2]] = [
+      this.data[index2],
+      this.data[index1]
+    ];
+  }
+
+  // O(1)
+  size() {
+    return this.data.length;
+  }
+}
 
 /** 
  * Your KthLargest object will be instantiated and called as such:
  * var obj = new KthLargest(k, nums)
  * var param_1 = obj.add(val)
  */
-
-[2, 3, 4, 4, 5, 5, 8, 9, 10]
