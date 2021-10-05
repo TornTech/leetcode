68a18f259cac46919805252ae67c383acf32707a
Ben Torn <tornben@gmail.com>
2021-10-04 18:14:46 -0700

    Time: 97 ms (30.81%), Space: 38.6 MB (70.20%) - LeetHub

--- integer-break/integer-break.js
+++ integer-break/integer-break.js
@@ -4,27 +4,32 @@
  */
 var integerBreak = function(n) {
     if (n <= 3) return n - 1;
-    if (n === 4) return n;
-    
-    let count2 = 0;
-    let count3 = Math.floor(n / 3);
-    let count4 = 0;
-
-    if (n % 3 === 1) {
-        count3--;
-        count4++;
-    } else if (n % 3 === 2) {
-        count2++;
-    }
     
     let product = 1;
-    while (count3 > 0) {
-        product *= 3
-        count3--;
-    }
+    let numOf3s = Math.floor(n / 3);
+    let remainder = n % 3;
     
-    if (count4) product *= 4;
-    if (count2) product *= 2;
+    product *= Math.pow(3, numOf3s);
+
+    if (remainder === 1) {
+        product /= 3;
+        product *= 4;
+    } else if (remainder > 0) {
+        product *= remainder;
+    }
     
     return product;
 };
+
+
+//  2 = 1 + 1,         product:  1
+//  3 = 2 + 1,         product:  2
+//  4 = 2 + 2,         product:  4
+//  5 = 2 + 3,         product:  6
+//  6 = 3 + 3,         product:  9
+//  7 = 3 + 4,         product: 12
+//  8 = 3 + 3 + 2,     product: 18
+//  9 = 3 + 3 + 3,     product: 27
+// 10 = 3 + 3 + 4,     product: 36
+// 11 = 3 + 3 + 3 + 2, product: 54
+
