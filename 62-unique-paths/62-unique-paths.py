ea8f7ef207c13819d58c481ac98318584cde89bb
Ben Torn <tornben@gmail.com>
2022-08-01 20:07:00 -0400

    Time: 34 ms (90.47%), Space: 13.8 MB (73.95%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -1,20 +1,10 @@
 class Solution:
     def uniquePaths(self, cols: int, rows: int) -> int:
-        seen = [[0 for i in range(rows)] for j in range(cols)]
+        seen = [[1 for i in range(rows)] for j in range(cols)]
         
-        for row in range(rows):
-            seen[0][row] = 1
-        for col in range(cols):
-            seen[col][0] = 1
-        
-        for col in range(cols):
-            for row in range(rows):
-                if (col == 0):
-                    seen[col][row] = seen[col][row-1]
-                elif (row == 0):
-                    seen[col][row] = seen[col-1][row]
-                else:
-                    seen[col][row] = seen[col-1][row] + seen[col][row-1]
+        for col in range(1, cols):
+            for row in range(1, rows):
+                seen[col][row] = seen[col-1][row] + seen[col][row-1]
 
         return seen[cols-1][rows-1]
         
