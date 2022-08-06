47de90436ad65d249b057a4cf0cabbde62477849
Ben Torn <tornben@gmail.com>
2022-08-06 17:36:44 -0400

    Time: 6303 ms (71.16%), Space: 13.9 MB (92.81%) - LeetHub

--- 79-word-search/79-word-search.py
+++ 79-word-search/79-word-search.py
@@ -1,44 +1,32 @@
-class Solution(object):
-    def exist(self, board, word):
-        """
-        :type board: List[List[str]]
-        :type word: str
-        :rtype: bool
-        """
-        self.ROWS = len(board)
-        self.COLS = len(board[0])
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        self.cols = len(board[0])
+        self.rows = len(board)
         self.board = board
-
-        for row in range(self.ROWS):
-            for col in range(self.COLS):
-                if self.backtrack(row, col, word):
+        
+        for col in range(self.cols):
+            for row in range(self.rows):
+                if self.traverse(col, row, word):
                     return True
-
-        # no match found after all exploration
+                
         return False
+                
 
-
-    def backtrack(self, row, col, suffix):
-        # bottom case: we find match for each letter in the word
-        if len(suffix) == 0:
+    def traverse(self, col, row, word):
+        if word == "":
             return True
-
-        # Check the current status, before jumping into backtracking
-        if row < 0 or row == self.ROWS or col < 0 or col == self.COLS \
-                or self.board[row][col] != suffix[0]:
+        
+        if (col < 0 or col == self.cols or row < 0 or row == self.rows or self.board[row][col] != word[0]):
             return False
-
-        ret = False
-        # mark the choice before exploring further.
-        self.board[row][col] = '#'
-        # explore the 4 neighbor directions
-        for rowOffset, colOffset in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
-            ret = self.backtrack(row + rowOffset, col + colOffset, suffix[1:])
-            # break instead of return directly to do some cleanup afterwards
-            if ret: break
-
-        # revert the change, a clean slate and no side-effect
-        self.board[row][col] = suffix[0]
-
-        # Tried all directions, and did not find any match
-        return ret
+        
+        self.board[row][col] = "#"
+        result = False
+        
+        for next_col,next_row in [(col-1, row),(col+1, row),(col, row-1),(col, row+1)]:
+            result = self.traverse(next_col, next_row, word[1:])
+            if result:
+                break
+                
+        self.board[row][col] = word[0]
+        
+        return result
