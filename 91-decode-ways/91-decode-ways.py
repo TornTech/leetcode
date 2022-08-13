b32730519577c7d2c96d86a1dd65a04a92b15b94
Ben Torn <tornben@gmail.com>
2022-08-12 21:18:52 -0400

    Time: 38 ms (84.30%), Space: 13.8 MB (80.84%) - LeetHub

---
+++ 91-decode-ways/91-decode-ways.py
@@ -0,0 +1,28 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        mem = [-1] * len(s)
+        
+        return self.numDecodingsHelper(s, 0, mem)
+    
+    def numDecodingsHelper(self, s: str, index: int, mem: list) -> int:
+        if index <= len(s) - 1 and s[index] == "0":
+            return 0
+        
+        if index > len(s) - 2:
+            return 1
+        
+        if mem[index] != -1:
+            return mem[index]
+        
+        first_digit  = s[index]
+        second_digit = s[index+1]
+        
+        if first_digit == "0":
+            mem[index] = self.numDecodingsHelper(s, index + 2, mem)
+            return mem[index]
+        elif int(first_digit + second_digit) <= 26:
+            mem[index] = self.numDecodingsHelper(s, index + 1, mem)  + self.numDecodingsHelper(s, index + 2, mem)
+            return mem[index]
+        else:
+            mem[index] = self.numDecodingsHelper(s, index + 1, mem)
+            return mem[index]
