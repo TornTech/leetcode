92f5655afec665ebb179da17f44d2da67d2dd8fc
Ben Torn <tornben@gmail.com>
2022-08-14 13:04:52 -0400

    Time: 40 ms (76.16%), Space: 14 MB (19.58%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -1,18 +1,14 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        self.mem = {}
-        rob_first  = self.robHelper(nums, 0)
-        rob_second = self.robHelper(nums, 1)
-        return max(rob_first, rob_second)
-    
-    def robHelper(self, nums: List[int], index: int) -> int: 
-        if index >= len(nums):
+        if not nums:
             return 0
-          
-        if index in self.mem:
-            return self.mem[index]
         
-        rob_current = self.robHelper(nums, index+2) + nums[index]
-        skip        = self.robHelper(nums, index+1)
-        self.mem[index] = max(rob_current, skip)
-        return self.mem[index]
+        maxRobbedAmount = [0] * (len(nums) + 1)
+        N = len(nums)
+        maxRobbedAmount[N-1] = nums[N-1]
+        
+        for i in range(N - 2, -1, -1):
+            maxRobbedAmount[i] = max(maxRobbedAmount[i+1], maxRobbedAmount[i+2] + nums[i])
+            
+        return maxRobbedAmount[0]
+        
